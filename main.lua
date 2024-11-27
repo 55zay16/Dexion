ca7169b74939490bb881627150be9cc6e5da9d0c
HiddenVue <167385155+HiddenVue@users.noreply.github.com>
2024-11-27 14:13:21 -0500

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,1 +1,5 @@
-
+if game.GameId == 11630038968 then
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/55zay16/Dexion/refs/heads/main/Games/bridge%20duel"))()
+else
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/55zay16/Dexion/refs/heads/main/Games/universal.lua"))()
+end
