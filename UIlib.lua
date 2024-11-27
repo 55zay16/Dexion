04af30b8f7c67b49462be824f0dfa974440242fb
HiddenVue <167385155+HiddenVue@users.noreply.github.com>
2024-11-27 15:25:06 -0500

    Update UIlib.lua

--- UIlib.lua
+++ UIlib.lua
@@ -106,4 +106,103 @@ function UIlib:Init(GameName)
     return DexionUI
 end
 
+function UIlib:AddTab(DexionUI,Tabtitle,XOFFSET,X,YOFFSET,Y) 
+    local Tab = Instance.new("TextLabel")
+    local UICorner = Instance.new("UICorner")
+    local Bar = Instance.new("Frame")
+    local Buttons = Instance.new("ScrollingFrame")
+    local UIListLayout = Instance.new("UIListLayout")
+    local Line = Instance.new("Frame")
+    
+    Tab.Name = Tabtitle
+    Tab.Parent = DexionUI
+    Tab.BackgroundColor3 = Color3.fromRGB(37, 37, 37)
+    Tab.BorderColor3 = Color3.fromRGB(0, 0, 0)
+    Tab.BorderSizePixel = 0
+    Tab.Position = UDim2.new(XOFFSET, X, YOFFSET, Y)
+    Tab.Size = UDim2.new(0, 200, 0, 50)
+    Tab.Font = Enum.Font.SourceSansSemibold
+    Tab.Text = Tabtitle
+    Tab.TextColor3 = Color3.fromRGB(255, 255, 255)
+    Tab.TextSize = 35.000
+    
+    UICorner.Parent = Tab
+    
+    Bar.Name = "Bar"
+    Bar.Parent = Tab
+    Bar.BackgroundColor3 = Color3.fromRGB(37, 37, 37)
+    Bar.BorderColor3 = Color3.fromRGB(0, 0, 0)
+    Bar.BorderSizePixel = 0
+    Bar.Position = UDim2.new(0, 0, 0.839999974, 0)
+    Bar.Size = UDim2.new(0, 200, 0, 8)
+    
+    Buttons.Name = "Buttons"
+    Buttons.Parent = Tab
+    Buttons.Active = true
+    Buttons.BackgroundColor3 = Color3.fromRGB(35, 35, 35)
+    Buttons.BorderColor3 = Color3.fromRGB(0, 0, 0)
+    Buttons.BorderSizePixel = 0
+    Buttons.Position = UDim2.new(0, 0, 1, 0)
+    Buttons.Size = UDim2.new(0, 200, 0, 484)
+    Buttons.HorizontalScrollBarInset = Enum.ScrollBarInset.ScrollBar
+    
+    UIListLayout.Parent = Buttons
+    UIListLayout.SortOrder = Enum.SortOrder.LayoutOrder
+    
+    Line.Name = "Line"
+    Line.Parent = Tab
+    Line.BackgroundColor3 = Color3.fromRGB(255, 0, 0)
+    Line.BorderColor3 = Color3.fromRGB(0, 0, 0)
+    Line.BorderSizePixel = 0
+    Line.Position = UDim2.new(0, 0, 0.959999979, 0)
+    Line.Size = UDim2.new(0, 200, 0, 1)
+end
+
+function UIlib:AddButton(Title,TargetTab,Callback)
+    local Button = Instance.new("TextButton")
+    local Line = Instance.new("Frame")
+    local Display = Instance.new("Frame")
+    local UICorner = Instance.new("UICorner")
+    
+    Button.Name = Title
+    Button.Parent = TargetTab.Buttons
+    Button.BackgroundColor3 = Color3.fromRGB(33, 33, 33)
+    Button.BorderColor3 = Color3.fromRGB(0, 0, 0)
+    Button.BorderSizePixel = 0
+    Button.Size = UDim2.new(0, 200, 0, 45)
+    Button.Font = Enum.Font.SourceSansSemibold
+    Button.TextColor3 = Color3.fromRGB(255, 255, 255)
+    Button.TextSize = 28.000
+    
+    Line.Name = "Line"
+    Line.Parent = Button
+    Line.BackgroundColor3 = Color3.fromRGB(255, 0, 0)
+    Line.BorderColor3 = Color3.fromRGB(0, 0, 0)
+    Line.BorderSizePixel = 0
+    Line.Position = UDim2.new(0, 0, 0.98222214, 0)
+    Line.Size = UDim2.new(0, 200, 0, 1)
+    
+    Display.Name = "Display"
+    Display.Parent = Button
+    Display.BackgroundColor3 = Color3.fromRGB(255, 0, 0)
+    Display.BorderColor3 = Color3.fromRGB(0, 0, 0)
+    Display.BorderSizePixel = 0
+    Display.Position = UDim2.new(0.0500000007, 0, 0.355555564, 0)
+    Display.Size = UDim2.new(0, 12, 0, 12)
+    
+    UICorner.CornerRadius = UDim.new(2, 0)
+    UICorner.Parent = Display
+
+    Button.MouseButton1Click:Connect(function()
+        if Display.BackgroundColor3 == Color3.fromRGB(255, 0, 0) then
+            Display.BackgroundColor3 = Color3.fromRGB(0, 255, 0)
+            Callback(true) -- enabled
+        else
+            Display.BackgroundColor3 = Color3.fromRGB(255, 0, 0)
+            Callback(false) -- not enabled
+        end
+
+    end)
+end
+
 return UIlib  
