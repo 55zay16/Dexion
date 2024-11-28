e647c19ebd3632e49f6c91d57a37d0d28ce451f9
HiddenVue <167385155+HiddenVue@users.noreply.github.com>
2024-11-28 17:01:41 -0500

    Update rivals.lua

--- Games/rivals.lua
+++ Games/rivals.lua
@@ -1,1 +1,17 @@
+local Players = game:GetService("Players")
+local LocalPlayer = Players.LocalPlayer
+local UserInputService = game:GetService("UserInputService")
+local UIlib = loadstring(game:HttpGet("https://raw.githubusercontent.com/55zay16/Dexion/refs/heads/main/UIlib.lua"))()
+
+Values = {
+
+}
+
+
+local Rivals = UIlib:Init("Rivals")
+
+local CombatTab = UIlib:AddTab(Rivals,"Combat",0.092,0,0.106,0) 
+local MovementTab = UIlib:AddTab(Rivals,"Movement",0.092 + 0.167,0,0.106,0)
+local VisualsTab = UIlib:AddTab(Rivals,"Visuals",0.092 + (0.167 * 2),0,0.106,0)
+local UtilitysTab = UIlib:AddTab(Rivals,"Utility",0.092 + (0.167 * 3),0,0.106,0)
 
