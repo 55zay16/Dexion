fd6e81a3418d99dc2ce43ca20d0df309412c3f3b
HiddenVue <167385155+HiddenVue@users.noreply.github.com>
2024-11-27 19:40:54 -0500

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,6 +1,6 @@
 if game.GameId == 4124008017 then
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/55zay16/Dexion/refs/heads/main/Games/bridge%20duel"))()
-else if GameId == then
+else if GameId == 3258873704 then
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/55zay16/Dexion/refs/heads/main/Games/skyways.lua"))()
 else
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/55zay16/Dexion/refs/heads/main/Games/universal.lua"))()
