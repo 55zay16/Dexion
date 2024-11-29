3b2feb89338cbc02062774c990bf3085b33160b4
HiddenVue <167385155+HiddenVue@users.noreply.github.com>
2024-11-29 14:22:11 -0500

    Update main.lua

--- main.lua
+++ main.lua
@@ -4,6 +4,8 @@ elseif game.GameId == 3258873704 then
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/55zay16/Dexion/refs/heads/main/Games/skywars.lua"))()
 elseif game.GameId == 1390601379 then
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/55zay16/Dexion/refs/heads/main/Games/combat%20warriors.lua"))()
+elseif game.GameId == 4777817887 then
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/55zay16/Dexion/refs/heads/main/Games/blade%20ball.lua"))()
 else
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/55zay16/Dexion/refs/heads/main/Games/universal.lua"))()
 end
